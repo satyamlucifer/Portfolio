6b3c4aaa11d175bb5e0a785bed809af0ff9e0177
Satyam Kumar <satyamrajarya@gmail.com>
2025-02-27 12:14:48 +0530

    added blogs changes

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/abu-said-bd/" className="text-[#16f2b3]">Satyam Kumar</Link>
+            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/satyam-kumar-here/" className="text-[#16f2b3]">Satyam Kumar</Link>
           </p>
           <div className="flex items-center gap-5">
             <Link
